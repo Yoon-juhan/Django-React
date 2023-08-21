8d1c443b87849a930aaaaf579879216572fb7b34
Yoon-juhan <108213439+Yoon-juhan@users.noreply.github.com>
2023-08-21 19:49:18 +0900

    2_commit

--- askcompany/instagram/urls.py
+++ askcompany/instagram/urls.py
@@ -1,22 +1,22 @@
 from django.urls import path, re_path, register_converter
 from . import views
-
-class Yearconverter:
-    regex = r"20\d{2}"
-
-    def to_python(self, value):
-        return int(value)
+from .converters import YearConverter, MonthConverter, DayConverter
     
-    def to_url(self, value):
-        return str(value)
-    
-register_converter(Yearconverter, 'year')
+register_converter(YearConverter, 'year')
+register_converter(MonthConverter, 'month')
+register_converter(DayConverter, 'day')
+
 
 app_name = 'instagram' # URL Reverse에서 namespace역할을 하게 된다.
 
 urlpatterns = [
     path('', views.post_list, name='post_list'), # views에 post_list 함수 호출 
     path('<int:pk>/', views.post_detail, name='post_detail'),
+    path('archive/', views.post_archive, name='post_archive'),
+    path('archive/<year:year>/', views.post_archive_year, name='post_archive_year'),
+    # path('archive/<year:year>/<month:month>/', views.post_archive_month, name='post_archive_month'),
+    # path('archive/<year:year>/<month:month>/<day:day>/', views.post_archive_day, name='post_archive_day'),
+
     # path('archives/<int:year>/', views.archives_year),
-    path('archives/<year:year>/', views.archives_year),
+    # path('archives/<year:year>/', views.archives_year),
 ]
--- askcompany/instagram/views.py
+++ askcompany/instagram/views.py
@@ -1,10 +1,23 @@
-from django.views.generic import ListView, DetailView
+
+from django.views.generic import ListView, DetailView, ArchiveIndexView, YearArchiveView
 from django.http import HttpRequest, HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils.decorators import method_decorator
 from .models import Post
 
-post_list = ListView.as_view(model=Post)
+# post_list = login_required(ListView.as_view(model=Post, paginate_by=10))
 
+# @method_decorator(login_required, name='dispatch')
+class PostListView(LoginRequiredMixin, ListView):
+    model = Post
+    paginate_by = 10
+
+post_list = PostListView.as_view()
+
+
+# @login_required
 # def  post_list(request):
 #     qs = Post.objects.all()     # 전부 가져옴
 #     q = request.GET.get('q', '')    # 
@@ -27,8 +40,23 @@ post_list = ListView.as_view(model=Post)
 #     })
 
 # CBV 클래스 기반 뷰
-post_detail = DetailView.as_view(model=Post)
+# post_detail = DetailView.as_view(model=Post, queryset=Post.objects.filter(is_public=True))
 
+class PostDetailView(DetailView):
+    model = Post
+    # queryset=Post.objects.filter(is_public=True)
 
-def archives_year(request, year):
-    return HttpResponse(f"{year}년 archives")
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if not self.request.user.is_authenticated:
+            qs = qs.filter(is_public=True)
+        return qs
+
+post_detail = PostDetailView.as_view()
+
+# def archives_year(request, year):
+#     return HttpResponse(f"{year}년 archives")
+
+post_archive = ArchiveIndexView.as_view(model=Post, date_field='created_at', paginate_by=10)
+
+post_archive_year = YearArchiveView.as_view(model=Post, date_field='created_at', make_object_list=True)
